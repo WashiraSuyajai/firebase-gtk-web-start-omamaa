d1664abb60777d6c8aad5463b5367448ed8290f0
WashiraSuyajai <washira.suyajai@gmail.com>
2020-04-23 11:40:30 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# firebase-gtk-web-start-omamaa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/firebase-gtk-web-start-omamaa)
---
+++ index.js
@@ -0,0 +1,60 @@
+// Import stylesheets
+import "./style.css";
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+import * as firebaseui from "firebaseui";
+
+// Document elements
+const startRsvpButton = document.getElementById("startRsvp");
+const guestbookContainer = document.getElementById("guestbook-container");
+
+const form = document.getElementById("leave-message");
+const input = document.getElementById("message");
+const guestbook = document.getElementById("guestbook");
+const numberAttending = document.getElementById("number-attending");
+const rsvpYes = document.getElementById("rsvp-yes");
+const rsvpNo = document.getElementById("rsvp-no");
+
+var rsvpListener = null;
+var guestbookListener = null;
+
+// Add Firebase project configuration object here
+var firebaseConfig = {
+  apiKey: "AIzaSyCYgNQnhKwxM_kDihs9HXpOVsZdTbt-wkE",
+  authDomain: "fir-web-codelab-7270e.firebaseapp.com",
+  databaseURL: "https://fir-web-codelab-7270e.firebaseio.com",
+  projectId: "fir-web-codelab-7270e",
+  storageBucket: "fir-web-codelab-7270e.appspot.com",
+  messagingSenderId: "810863378766",
+  appId: "1:810863378766:web:ec1a5bd8ef2472e04f1e1f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// var firebaseConfig = {};
+
+// firebase.initializeApp(firebaseConfig);
+
+// FirebaseUI config
+const uiConfig = {
+  credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+  signInOptions: [
+    // Email / Password Provider.
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+  callbacks: {
+    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+      // Handle sign-in.
+      // Return false to avoid redirect.
+      return false;
+    }
+  }
+};
+
+// const ui = new firebaseui.auth.AuthUI(firebase.auth());
+// Initialize the FirebaseUI widget using Firebase
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
